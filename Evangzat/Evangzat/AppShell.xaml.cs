306d8c4c989fb9d43377853d57e06c70635e255a
nelsallg <91795988+Nelsallg@users.noreply.github.com>
2023-04-18 16:28:03 +0300

    first commit

---
+++ Evangzat/Evangzat/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Evangzat.ViewModels;
+using Evangzat.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Evangzat
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Evangzat/Evangzat/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Evangzat.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Evangzat.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
